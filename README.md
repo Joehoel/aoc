7112db082f2624e5122959b25512bf078ab1f289
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-12-01 16:36:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2021
+
+## All code for advent of code 2021
+
+---
+
+Joël Kuijper
---
+++ day-1/part-1/main.ts
@@ -0,0 +1,20 @@
+const input = Deno.readTextFileSync("day-1/input.txt").split("\n").map(Number);
+
+export const count = (arr: number[]) =>
+    arr.reduce((acc, curr, index, array) => {
+        // count the number of times a measurement (curr) increases from the previous measurement (acc)
+        if (index == 0) {
+            console.log(`${curr} (N/A - no previous measurement)`);
+            return acc;
+        }
+        if (curr > array[index - 1]) {
+            console.log(`${curr} (increased)`);
+            return acc + 1;
+        } else if (curr == array[index - 1]) {
+            console.log(`${curr} (N/A - no change)`);
+            return acc;
+        }
+        return acc;
+    }, 0);
+
+console.log(count(input));
