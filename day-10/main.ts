ee2ed3fb9002e5389a2afb1bb2e6fc8081b4b183
Joël Kuijper <jmkuijper@outlook.com>
2021-12-10 10:41:21 +0100

    day-10 update

--- day-10/main.ts
+++ day-10/main.ts
@@ -2,21 +2,16 @@ import { lines } from "../utils.ts";
 
 const input = lines("day-10/input.txt").map(line => line.split(""));
 
-const opening = ["{", "[", "(", "<"];
-const closing = ["}", "]", ")", ">"];
-
 // loop over all tags and check if there is an opening tag, then loop over the rest until the corresponding closing tag is found, if its not found, the line is corrupt and need to be removed.
 class Subsystem {
-    public chunks: string[][] = [];
-    public legalChunks: string[][] = [];
+    private readonly opening = ["{", "[", "(", "<"];
+    private readonly closing = ["}", "]", ")", ">"];
+
+    private chunks: string[][];
 
     constructor(chunks: string[][]) {
         this.chunks = chunks;
-        this.findChunks();
     }
-
-    public findChunks() {}
 }
 
 const ss = new Subsystem(input);
-console.log(ss.legalChunks);
