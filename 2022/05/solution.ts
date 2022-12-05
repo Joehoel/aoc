3e81e7a98185a11ce251a13297a23dc5dde02772
Joël Kuijper <jmkuijper@outlook.com>
2022-12-05 11:29:59 +0100

    feat: working 05

--- 2022/05/solution.ts
+++ 2022/05/solution.ts
@@ -8,7 +8,7 @@ const print = (stacks: string[][]) => {
   for (let i = 0; i < 10; i++) {
     let row = "";
     for (let j = 0; j < stacks.length; j++) {
-      row += stacks[j][i] ?? " ";
+      row += stacks[j][i] ?? "   ";
     }
     console.log(row);
   }
@@ -24,7 +24,7 @@ function one(input: string): string {
   const toStack = groups[0].split("\n").reverse().slice(1);
 
   for (let i = 0; i < (toStack[0].length + 1) / 4; i++) {
-    stacks.push(toStack.map(line => line.slice(i * 4, i * 4 + 3)));
+    stacks.push(toStack.map(line => line.slice(i * 4, i * 4 + 3).trim()).filter(Boolean));
   }
 
   // get all the digits from the string into an array
@@ -39,8 +39,6 @@ function one(input: string): string {
     .filter(s => s.length > 0);
 
   // move 'number' amount of items from the top of 'from' stack to the top of 'to' stack
-
-  print(stacks);
 
   for (const instruction of instructions) {
     const count = instruction[0];
@@ -60,7 +58,56 @@ function one(input: string): string {
 }
 
 function two(input: string): string {
-  return "0";
+  const groups = input.split("\n\n");
+
+  // if a line in the stacks string contains numbers, remove the line. remove all brackets [] and make  a grid of the remaining lines.
+  const stacks: string[][] = [];
+  // const stacks = [["Z", "N"], ["M", "C", "D"], ["P"]];
+
+  const toStack = groups[0].split("\n").reverse().slice(1);
+
+  for (let i = 0; i < (toStack[0].length + 1) / 4; i++) {
+    stacks.push(toStack.map(line => line.slice(i * 4, i * 4 + 3).trim()).filter(Boolean));
+  }
+
+  // get all the digits from the string into an array
+  const instructions = groups[1]
+    .split("\n")
+    .map(line =>
+      line
+        .split(" ")
+        .filter(l => l.match(/\d+/g))
+        .map(Number)
+    )
+    .filter(s => s.length > 0);
+
+  // move 'number' amount of items from the top of 'from' stack to the top of 'to' stack
+
+  //  for (const instruction of instructions) {
+  //    const count = instruction[0];
+  //    const from = instruction[1] - 1;
+  //    const to = instruction[2] - 1;
+
+  //    for (let i = 0; i < count; i++) {
+  //      stacks[to].push(stacks[from].pop() ?? "");
+  //    }
+  //  }
+  // instead of executing the instructions one by one we can just move them all (count) and keep the order that they are in
+
+  for (const instruction of instructions) {
+    const count = instruction[0];
+    const from = instruction[1] - 1;
+    const to = instruction[2] - 1;
+
+    stacks[to].push(...stacks[from].splice(-count));
+  }
+
+  const result = stacks
+    .map(s => s[s.length - 1])
+    .join("")
+    .replace(/[\[\]]/g, "");
+
+  return result;
 }
 
 Deno.test("Part 1", () => {
@@ -72,8 +119,8 @@ Deno.test("Part 1", () => {
 Deno.test("Part 2", () => {
   const result = two(example);
 
-  assertEquals(result, "");
+  assertEquals(result, "MCD");
 });
 
 console.log(`Part 1: `, one(input));
-// console.log(`Part 2: `, two(input));
+console.log(`Part 2: `, two(input));
