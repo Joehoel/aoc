d1ace4411795e7a7094d854b5c7757e799e86c0b
Joël Kuijper <joel@deindruk.nl>
2022-12-05 16:36:08 +0100

    feat: setup day 6

---
+++ 2022/06/solution.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "testing/asserts.ts";
+import { raw } from "utils";
+import { one, two } from "./solution.ts";
+
+const example = raw("2022/06/example.txt");
+
+Deno.test("Part 1", () => {
+  const result = one(example);
+
+  assertEquals(result, undefined);
+});
+
+Deno.test("Part 2", () => {
+  const result = two(example);
+
+  assertEquals(result, undefined);
+});
---
+++ 2022/06/solution.ts
@@ -0,0 +1,14 @@
+import { raw } from "utils";
+
+const input = raw("2022/06/input.txt");
+
+export function one(input: string) {
+  return undefined;
+}
+
+export function two(input: string) {
+  return undefined;
+}
+
+console.log(`Part 1: `, one(input));
+console.log(`Part 2: `, two(input));
