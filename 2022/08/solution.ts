4d3fb546d454b62f9ab32c343a14f91dcc7a55c4
Joël Kuijper <jmkuijper@outlook.com>
2022-12-07 09:52:51 +0100

    feat: add plop

---
+++ 2022/08/solution.ts
@@ -0,0 +1,13 @@
+import { raw } from "utils";
+
+const input = raw("2022/8/input.txt");
+
+export function one(input: string): number {
+  return 0;
+}
+export function two(input: string): number {
+  return 0;
+}
+
+console.log(`Part 1: `, one(input));
+console.log(`Part 2: `, two(input));
