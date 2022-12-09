d300be1552dcd41b9ffa7a283d0368157452219b
Joël Kuijper <jmkuijper@outlook.com>
2022-12-09 11:47:51 +0100

    feat: restore 6

---
+++ 2022/06/solution.ts
@@ -0,0 +1,30 @@
+import { raw } from "utils";
+
+const input = raw("2022/06/input.txt");
+
+function detectMarker(sequence: string, length: number) {
+  const chars = sequence
+    .split("")
+    .map(c => c.trim())
+    .filter(Boolean);
+
+  // find the index where there is a sequence of 4 unique letters
+  const marker = chars.findIndex((_, i) => {
+    const slice = chars.slice(i, i + length);
+
+    return new Set(slice).size === length;
+  });
+
+  return marker + length;
+}
+
+export function one(input: string) {
+  return detectMarker(input, 4);
+}
+
+export function two(input: string) {
+  return detectMarker(input, 14);
+}
+
+console.log(`Part 1: `, one(input));
+console.log(`Part 2: `, two(input));
