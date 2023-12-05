1bce5df537bd1ba01c5a34e344f845e9c9ea136c
Joël Kuijper <jmkuijper@outlook.com>
2023-12-05 09:23:01 +0100

    feat: create day 5

---
+++ 2023/05/solution.ts
@@ -0,0 +1,15 @@
+import { raw } from "utils";
+
+const input = raw("2023/05/input.txt");
+
+export function one(input: string): number {
+  return 0;
+}
+export function two(input: string): number {
+  return 0;
+}
+
+console.table({
+  "Part 1": one(input),
+  "Part 2": two(input),
+});
